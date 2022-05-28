792998e4cd12bf5c37020d226f6ec485e8c16a04
LaffSteven <steve.laff@gmail.com>
2022-05-28 11:50:24 -0400

    added router test calls

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ import {useState, useEffect} from 'react';
 const App = () => {
 
     const [dbMessage, setDbMessage] = useState()
+    const [cardMessage, setCardMessage] = useState()
+    const [deckMessage, setDeckMessage] = useState()
 
 
     useEffect(()=>{
-        axios.get('https://magic-academy-api.herokuapp.com/testing/db/connection-test')
+        axios.get('https://magic-academy-api.herokuapp.com/')
             .then((response) => {
                 setDbMessage(response.data);
-            })
+        })
+        axios.get('https://magic-academy-api.herokuapp.com/cards/')
+            .then((response) => {
+                setCardMessage(response.data);
+        })
+        axios.get('https://magic-academy-api.herokuapp.com/decks/')
+            .then((response) => {
+                setDeckMessage(response.data);
+        })
     })
 
 
@@ -20,6 +30,9 @@ const App = () => {
             <h1>Helo World!</h1>
 
             <p>{dbMessage}</p>
+            <p>{cardMessage}</p>
+            <p>{deckMessage}</p>
+
         </>
     )
 }
