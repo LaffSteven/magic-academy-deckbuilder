c60bda07e7a163fcbb7e3813c7b431ee63897f36
LaffSteven <steve.laff@gmail.com>
2022-05-31 14:59:27 -0400

    new card component

---
+++ src/components/cards/NewCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const NewCard = () => {
+
+    const [newCard, setNewCard] = useState({})
+
+
+    // axios.post('/')
+
+    return(
+        <></>
+    )
+
+}
+
+export default NewCard
